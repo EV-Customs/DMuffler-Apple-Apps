519c0d43c07a09f2227724d01569bfd3ac586984
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2026-08-24 20:55:08 -0700

    Start input error check for phone number

--- DMuffler-Apple-Apps/DMuffler/Models/User.swift
+++ DMuffler-Apple-Apps/DMuffler/Models/User.swift
@@ -4,7 +4,14 @@
 //  Created by Blaze Sanders on 2026-08-23
 //  For support email dev@evcustoms.store
 
+// Standard libraries
+import SwiftData                // Instead of "import Foundation", "SQLite" or "GRDB" (feature-rich SQLite toolkit in Swift)
 import Foundation
+import DeveloperToolsSupport    // Used to define images in "VehicleAsset" struct
+
+
+// External libraries
+
 
 /**
  Represent a user profile and their associated registered vehicles within a DMuffler app.
@@ -21,4 +28,20 @@ struct User: Identifiable{
     var firstName: String
     var phoneNumber: UInt32
     var vehicles: [String]
+}
+
+
+
+/**
+ Validates whether a given string is a valid USA, Mexico, Canada, Japan, Germany (no production database should ever store an invalid phone number).
+
+ - Parameter input: The phone number string entered by a user.
+ - Returns: `true` if the phone number is valid; otherwise, `false`.
+ */
+func isValidPhoneNumber(input: String) -> Bool {
+    let validPhoneNumberharacters = CharacterSet(charactersIn: "0123456789+.-()")  // Excludes (I, O , and Q)
+    
+
+    // Check length and ensure uppercase matching against valid characters
+    return input.count == 9 && input.uppercased().unicodeScalars.allSatisfy { validPhoneNumberharacters.contains($0) }
 }
